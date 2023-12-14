c50c2935aedf7c39e56a5cb73b5bbe360bb67858
unknown <tinhtrinh54@gmail.com>
2023-12-14 22:01:26 +0700

    Add CustonScpllbas and add react-slider-carousel

--- Reactjs/src/containers/HomePage/HomePage.js
+++ Reactjs/src/containers/HomePage/HomePage.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import HomeHeader from "./HomeHeader";
+import Specialty from "./Section/Specialty";
 class HomePage extends Component {
   render() {
-    return <div> <HomeHeader/></div>;
+    return (
+      <div>
+        <HomeHeader />
+        <Specialty />
+      </div>
+    );
   }
 }
 
